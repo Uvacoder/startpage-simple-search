e811c005ede94e84963ef1a72361dc12ed599471
EmDev21 <103883116+EmDev21@users.noreply.github.com>
2022-04-17 23:23:39 +0200

    Fixed reddit could not go website without args

--- src/js/commands.js
+++ src/js/commands.js
@@ -1,5 +1,5 @@
 function google(cmd) {
-    if (!(cmd == undefined)) {
+    if (cmd != "google") {
         str = "";
         var parameters = cmd.split(" ").slice(1);
         for (var i = 0; i < parameters.length; i++) {
@@ -12,7 +12,7 @@ function google(cmd) {
 }
 
 function reddit(cmd) {
-    if (!(cmd == undefined)) {
+    if (cmd != "reddit") {
         window.open("https://www.reddit.com/r/" + cmd.split(" ").slice(1)[0]);
     } else {
         window.open("https://www.reddit.com/");
